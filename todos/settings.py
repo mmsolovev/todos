825cfa4b01ba76c8297ba3fc8fcbc6edefcf24d3
mmsolovev <mmsolovevmail@gmail.com>
2022-01-30 16:36:35 +0300

    lesson_1: made app userapp

--- todos/settings.py
+++ todos/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'userapp',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth
+
+AUTH_USER_MODEL = 'userapp.User'
