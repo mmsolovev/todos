0f53c78dc07341ea563149231a3f5dfcdf4fab46
mmsolovev <mmsolovevmail@gmail.com>
2022-01-31 19:50:16 +0300

    lesson_3: made API for todos and projects

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import ProjectSerializer, ToDoSerializer
+from .models import Project, ToDo
 
-# Create your views here.
+
+class ProjectViewSet(ModelViewSet):
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
+
+
+class ToDoViewSet(ModelViewSet):
+    serializer_class = ToDoSerializer
+    queryset = ToDo.objects.all()
