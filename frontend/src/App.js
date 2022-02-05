5cb9de85d29aaa4ccdb972f35dda8b262b5db6ae
mmsolovev <mmsolovevmail@gmail.com>
2022-02-05 22:11:44 +0300

    lesson_5: work with frontend, routing, SPA for users, projects, ToDos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import axios from "axios";
-import Menu from "./components/Menu";
+import Navbar from "./components/Menu";
 import Footer from "./components/Footer";
 import UserList from "./components/User";
+import ToDoList from "./components/ToDo";
+import {ProjectDetail, ProjectList} from "./components/Project";
+
+
+const DOMAIN = 'http://127.0.0.1:8000'
+const get_url = (url) => `${DOMAIN}${url}`
 
 
 class App extends React.Component {
@@ -12,36 +18,57 @@ class App extends React.Component {
    constructor(props) {
        super(props)
        this.state = {
-           'authors': [],
-           'users': []
+           navbarItems: [
+                {name: 'Users', href: '/'},
+                {name: 'Projects', href: '/projects'},
+                {name: 'TODOs', href: '/todos'},
+            ],
+           users: [],
+           projects: [],
+           project: {},
+           todos: []
        }
    }
 
    componentDidMount() {
-   axios.get('http://127.0.0.1:8000/api/users')
-       .then(response => {
-           const users = response.data
-               this.setState(
-               {
-                   'users': users
-               }
-           )
-       }).catch(error => console.log(error))
-   }
+        axios.get(get_url('/api/users/'))
+        .then(response => {
+            //console.log(response.data)
+            this.setState({users: response.data.results})
+        }).catch(error => console.log(error))
+
+        axios.get(get_url('/api/projects/'))
+        .then(response => {
+            //console.log(response.data)
+            this.setState({projects: response.data.results})
+        }).catch(error => console.log(error))
+
+        axios.get(get_url('/api/todos/'))
+        .then(response => {
+            //console.log(response.data)
+            this.setState({todos: response.data.results})
+        }).catch(error => console.log(error))
+    }
 
    render () {
        return (
-           <div>
-               <div>
-                   <Menu />
-               </div>
-               <div>
-                   <UserList users={this.state.users} />
-               </div>
-               <div>
-                   <Footer />
-               </div>
-           </div>
+           <BrowserRouter>
+                  <header>
+                    <Navbar navbarItems={this.state.navbarItems} />
+                  </header>
+                  <main>
+                      <div>
+                        <Routes>
+                            <Route path='/' element={<UserList users={this.state.users} />} />
+
+                            <Route path='/projects' element={<ProjectList items={this.state.projects} />} />
+
+                            <Route path='/todos' element={<ToDoList items={this.state.todos} />} />
+                        </Routes>
+                      </div>
+                  </main>
+              <Footer />
+            </BrowserRouter>
        )
    }
 }
