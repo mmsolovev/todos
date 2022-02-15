8a04e64c223229972cca1496a6abc53202a325a8
mmsolovev <mmsolovevmail@gmail.com>
2022-02-15 12:44:51 +0300

    lesson_9: made documentation for API with drf-yasg

--- todos/urls.py
+++ todos/urls.py
@@ -1,20 +1,8 @@
-"""todos URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from userapp.views import UserViewSet
@@ -25,10 +13,25 @@ router.register('users', UserViewSet)
 router.register('projects', ProjectViewSet)
 router.register('todos', ToDoViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="todos",
+      default_version='0.1',
+      description="Documentation to out project",
+      contact=openapi.Contact(email="admin@admin.local"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
     re_path(r'^api/(?P<version>\d\.\d)/users/$', UserViewSet.as_view({'get': 'list'})),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
